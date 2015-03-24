a555d717d82da4bd726814faa53c3abf15009cb4
Miguel Ramos <miguel.marques.ramos@gmail.com>
2015-03-24 18:41:55 +0000

    Added module body-parser for test on express as dev.  Express middleware browser define a property called current on browser instance to define current folder to list. Added more express tests for middleware.

--- test/express/express.js
+++ test/express/express.js
@@ -2,7 +2,8 @@ var should  = require('should'),
     path    = require('path'),
     express = require('express'),
     request = require('supertest'),
-    Browser = require('../../');
+    Browser = require('../../'),
+    bodyParser = require('body-parser');
 
 describe('#express', function(){
     it('Should request have property browser with value folder instance.', function(done){
@@ -21,17 +22,100 @@ describe('#express', function(){
         request(app).get('/browser').expect(200, done);
     });
 
-    /*it('GET /browser', function(done){
+    it('GET /browser?root=/', function(done){
         var app = express();
 
+        app.use(Browser.express({
+            home: path.join(__dirname, '..', 'home')
+        }));
+
         app.get('/browser', function(req, res){
-            res.status(200).send({ name: 'tobi' });
+            var browser = req.browser;
+
+            browser.open(browser.current, function(list){
+
+                list.should.have.property('files');
+
+                (list['files'].length).should.be.above(0);
+
+                res.status(200).send(list);
+            });
         });
 
-        request(app)
-            .get('/browser')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200, done);
-    });*/
+        request(app).get('/browser?root=%2F').expect(200, done);
+    });
+
+    it('GET /browser?root=/mydocs', function(done){
+        var app = express();
+
+        app.use(Browser.express({
+            home: path.join(__dirname, '..', 'home')
+        }));
+
+        app.get('/browser', function(req, res){
+            var browser = req.browser;
+
+            browser.open(browser.current, function(list){
+
+                list.should.have.property('files');
+
+                (list['files'].length).should.be.above(0);
+
+                res.status(200).send(list);
+            });
+        });
+
+        request(app).get('/browser?root=%2Fmydocs').expect(200, done);
+    });
+
+    it('GET /browser?root=/.secret', function(done){
+        var app = express();
+
+        app.use(Browser.express({
+            home: path.join(__dirname, '..', 'home')
+        }));
+
+        app.get('/browser', function(req, res){
+            var browser = req.browser;
+
+            browser.open(browser.current, function(list){
+
+                list.should.have.property('files');
+
+                (list['files'].length).should.be.above(0);
+
+                res.status(200).send(list);
+            });
+        });
+
+        request(app).get('/browser?root=%2F.secret').expect(200, done);
+    });
+
+    it('POST /browser/find?root=/', function(done){
+        var app = express();
+
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: true }));
+
+        app.use(Browser.express({
+            home: path.join(__dirname, '..', 'home')
+        }));
+
+        app.post('/browser/find', function(req, res){
+            var browser = req.browser;
+
+            browser.open(browser.current, function(list){
+                var search = req.body.hasOwnProperty('search') ? req.body.search : null;
+
+                var pdf = browser.find(search),
+                    status = pdf ? 200 : 400;
+
+                pdf.should.have.property('type');
+
+                res.status(status).send(pdf);
+            });
+        });
+
+        request(app).post('/browser/find?root=%2F').send({"search": "pdf"}).expect(200, done);
+    });
 });
