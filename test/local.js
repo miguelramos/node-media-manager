680fea4f29dde220fa091e904c20e953bba8133f
Miguel Ramos <miguel.marques.ramos@gmail.com>
2015-03-27 16:00:51 +0000

    Added method to local to add files to home directory. Tests implemented.

--- test/local.js
+++ test/local.js
@@ -53,7 +53,7 @@ describe('#local', function(){
 
             done();
         });
-        debugger;
+
         folder.open('../../../');
     });
 
@@ -127,5 +127,87 @@ describe('#local', function(){
         });
 
         folder.open("/");
+    });
+
+    it('Should add file to home with callback response.', function(done){
+        var folder = new Local(path.join(__dirname, 'home')),
+            image  = path.join(__dirname, 'fixtures', 'code-wallpaper-power.jpg');
+
+        folder.add(image, 'myfolder', function(error, file){
+            file.should.have.property('name');
+            file.should.have.property('path');
+            file.should.have.property('ext');
+            file.should.have.property('rel');
+
+            done();
+        });
+    });
+
+    it('Should add file to home with event response.', function(done){
+        var folder = new Local(path.join(__dirname, 'home')),
+            image  = path.join(__dirname, 'fixtures', 'code-wallpaper-java.png');
+
+        folder.on('onAddFile', function(err, file){
+            file.should.have.property('name');
+            file.should.have.property('path');
+            file.should.have.property('ext');
+            file.should.have.property('rel');
+
+            done();
+        });
+
+        folder.add(image, 'myfolder');
+    });
+
+    it('Should test add file possible security breach on callback.', function(done){
+        var folder = new Local(path.join(__dirname, 'home')),
+            image  = path.join(__dirname, 'fixtures', 'code-wallpaper-java.png');
+
+        folder.add(image, '../../../', function(error, file){
+            process.nextTick(function () {
+                error.message.should.equal("Permission denied to access folder outside home.");
+
+                done();
+            });
+        });
+    });
+
+    it('Should test add file possible security breach on event.', function(done){
+        var folder = new Local(path.join(__dirname, 'home')),
+            image  = path.join(__dirname, 'fixtures', 'code-wallpaper-java.png');
+
+        folder.on('onReadFile', function(err, file){
+            err.message.should.equal("Permission denied to access folder outside home.");
+
+            done();
+        });
+
+        folder.add(image, '../../../');
+    });
+
+    it('Should test file do not exist with callback.', function(done){
+        var folder = new Local(path.join(__dirname, 'home')),
+            image  = path.join(__dirname, 'fixtures', 'xpto.png');
+
+        folder.add(image, 'mydocs', function(error, file){
+            process.nextTick(function () {
+                error.code.should.eql('ENOENT');
+
+                done();
+            });
+        });
+    });
+
+    it('Should test file do not exist with event.', function(done){
+        var folder = new Local(path.join(__dirname, 'home')),
+            image  = path.join(__dirname, 'fixtures', 'xpto.png');
+
+        folder.on('onReadFile', function(err, file){
+            err.code.should.eql('ENOENT');
+
+            done();
+        });
+
+        folder.add(image, 'mydocs');
     });
 });
