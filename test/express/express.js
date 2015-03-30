21ad346340fbb173291a8ef65b792fe59dc6cab5
Miguel Ramos <miguel.marques.ramos@gmail.com>
2015-03-30 18:28:34 +0100

    Cannot be file to dir. It seems size of file that rename method of fs don't like it. Debugging more about it.

--- test/express/express.js
+++ test/express/express.js
@@ -298,17 +298,16 @@ describe('#express', function(){
             browser.move(from, to, function(err, rs){
                 if(err){
                     console.log(err);
-                    console.log(rs);
                 }
 
                 rs.should.have.property('from', path.join(__dirname, '..', 'home', 'wallpaper.jpg'));
-                rs.should.have.property('to', path.join(__dirname, '..', 'home', 'mydocs', 'wallpaper.jpg'));
+                rs.should.have.property('to', path.join(__dirname, '..', 'home', 'mydocs', 'power.jpg'));
 
                 res.status(200).send(rs);
             });
         });
 
-        request(app).put('/browser/move?src=wallpaper.jpg&dst=mydocs%2F').expect(200, done);
+        request(app).put('/browser/move?src=wallpaper.jpg&dst=mydocs%2Fpower.jpg').expect(200, done);
     });
 
     it('PUT /browser/link', function(done){
